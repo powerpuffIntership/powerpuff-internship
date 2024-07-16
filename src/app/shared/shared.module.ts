339022ccb169188d62504a6dda6c275819b6fffc
rudzkabeata <beata.rudzka@vattenfall.com>
2024-07-16 14:17:46 +0200

    create footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CustomBtnComponent } from './components/custom-btn/custom-btn.component';
+import { LegendComponent } from './components/legend/legend.component';
 
 
 @NgModule({
-  declarations: [ HeaderComponent, FooterComponent ],
+  declarations: [ HeaderComponent, FooterComponent, CustomBtnComponent, LegendComponent ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CustomBtnComponent,
+    LegendComponent
   ]
 })
 export class SharedModule { }
