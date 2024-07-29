8b40bae726f96554819cbd42d093d48f01485b0a
joannapienko <joanna.pienko@vattenfall.com>
2024-07-29 15:40:50 +0200

    Added translate to status

--- src/app/pages/welcome-page/components/status/status.component.ts
+++ src/app/pages/welcome-page/components/status/status.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { SafetyStatusModel } from '../../../../core/models/safetyStatus.model';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './status.component.html',
   styleUrl: './status.component.scss',
 })
