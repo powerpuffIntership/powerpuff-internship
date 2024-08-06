9fe9de76abe4cabaaf78649bb0e387047e829b92
Golabek Pawel (YIFO) ext <cgk33@eur.corp.vattenfall.com>
2024-08-06 13:22:15 +0200

    changed icon and colors, todo: change cursor on homepage and make arrow clickable

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { State } from '@ngxs/store';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  ngOnInit() {
+    let location:string = window.location.href;
+    if(location.includes("reactors")){
+      var htmlElement;
+      var inactiveElement;
+      
+      if(htmlElement = document.getElementById("headerIcon")){
+        htmlElement.className = "header-icon__wrapper__reactorIcon";
+        htmlElement.setAttribute("href","#welcome")
+     //   htmlElement.setAttribute("onclick","#welcome")
+      }
+    }else{
+      if(inactiveElement = document.getElementById("wrapperLink")){
+        inactiveElement.setAttribute("cursor", "default");
+        inactiveElement.setAttribute("disabled", "disabled");
+      }
+    }
+  }
 }
